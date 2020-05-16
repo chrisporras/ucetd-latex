22830ddf257383e05d5b0ec37e686b48b3489b18
ChristianPorras <40405005+chrisporras@users.noreply.github.com>
2020-05-16 18:56:44 -0500

    finished methods up to sampling

---
+++ src/sample.py
@@ -0,0 +1,19 @@
+    #!python3
+"""
+    Performs convolution of frequency matrix
+    with Gaussian sampling kernel of width w
+    Returns: F matrix of sampled allele frequencies
+"""
+import numpy as np
+from scipy.ndimage import gaussian_filter
+
+def sample_f(f,w):
+    F = np.zeros(f.shape)
+    ## Need to convolve array along spatial axes
+    ## so iterate over time axis
+    for i in range(f.shape[0]):
+        F[i] = gaussian_filter(f[i],sigma=w,mode = "wrap")
+    return F
+
+if __name__ == "__main__":
+    sample_f()
